2da1e6eba723f6f8ac23dc9bff8e34abaf08b481
Igor <romanigor.conect@gmail.com>
2022-08-24 23:43:09 +0200

    changed the elements in array

---
+++ ArrayanotherElements/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] myArray2 = {0,0,0,0,0,0,0,0};
+//int[] myArray3 = Enumerable.Repeat(1,8).ToArray();
+//for(int i = 0; i<myArray2.Length; i++)
+//{
+    //Console.WriteLine(myArray2[i]);
+   // break;
+//}
+myArray2[0]  = 1;
+myArray2[1]  = 1;
+myArray2[2]  = 1;
+myArray2[3]  = 1;
+myArray2[4]  = 1;
+myArray2[5]  = 1;
+myArray2[6]  = 1;
+myArray2[7]  = 1;
+for(int i = 0; i<myArray2.Length; i++)
+{
+    Console.WriteLine(myArray2[i]);
+}
