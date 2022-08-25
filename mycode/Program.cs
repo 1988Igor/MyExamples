d35568840b15cb89e335a583c6b8ec44e0df7b47
Igor <romanigor.conect@gmail.com>
2022-08-25 22:33:50 +0200

    trainig with array

---
+++ mycode/Program.cs
@@ -0,0 +1,37 @@
+﻿int[]myArray = new int[12];
+Random rand = new Random();
+int i, positive, negative, po, ne;
+i = positive=negative  = po = ne = 0;
+
+    for(i=0;i<myArray.Length; i++)
+    myArray[i] = rand.Next(-2,9);
+
+    for(i=0;i<myArray.Length; i++)
+{
+    Console.WriteLine(myArray[i]);
+    
+    if (myArray[i]>=0)
+    po+=myArray[i];
+
+    if (myArray[i]<0)
+    ne+=myArray[i];
+   
+}
+
+    for(i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i]>=0)
+        positive++;
+
+        else
+        negative++;
+    }
+
+
+
+    Console.WriteLine("The positive elements of array are :" + positive);
+    Console.WriteLine("The negative elements of array are :" + negative);
+    
+    Console.WriteLine("The sum of positive elements of array is " + po );
+    Console.WriteLine("The sum of negative elements of array is " + ne );
+    
