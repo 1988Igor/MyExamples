a651eef7509fbe279838a93a8cfd82efbeea0971
Igor <romanigor.conect@gmail.com>
2022-08-27 14:18:42 +0200

    calculator

---
+++ Mytrainig/Program.cs
@@ -0,0 +1,29 @@
+﻿double firstvalue, secondvalue;
+string action;
+ 
+ Console.WriteLine("Enter the first number :");
+ firstvalue = double.Parse(Console.ReadLine());
+ Console.WriteLine("Enter the second number :");
+ secondvalue = double.Parse(Console.ReadLine());
+ Console.WriteLine("Chose a symbol : '+', '-', '*' ");
+ action = Console.ReadLine();
+
+ switch(action)
+ {
+
+    case "+":
+        Console.WriteLine(firstvalue+secondvalue);
+        break;
+
+    case "-":
+    Console.WriteLine(firstvalue-secondvalue);
+    break;
+
+    case "*":
+    Console.WriteLine(firstvalue*secondvalue);
+    break;
+    default:
+    Console.WriteLine("Wrong symbol, please try again!");
+    break;
+
+ }
