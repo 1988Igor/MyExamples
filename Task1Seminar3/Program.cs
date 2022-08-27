0c0d36a4277b63ab00069fc428dbe112be8d56b3
Igor <romanigor.conect@gmail.com>
2022-08-27 14:53:44 +0200

    table of power

---
+++ Task1Seminar3/Program.cs
@@ -0,0 +1,16 @@
+﻿void Fillnumber (int number1)
+{
+int count  = 1;
+while (count<=number1)
+{
+
+
+Console.WriteLine($"{count}*{count} = {count*count}");
+count++;
+}
+
+}
+
+Console.WriteLine("Enter the number N :");
+int number  = int.Parse(Console.ReadLine());
+Fillnumber(number);
