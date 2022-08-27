8b71e347b198832e54934cffe7757d572b91ce93
Igor <romanigor.conect@gmail.com>
2022-08-27 19:47:56 +0200

    Определить количество цифер в числе

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿//Найти сумму чисел от 1 до А
+
+//void Fillnumber (int number1)
+//{
+//int count  = 1;
+//while (count<=number1)
+//{
+
+
+//Console.WriteLine($"{count+count+count}");
+//count++;
+//}
+
+//}
+
+//Console.WriteLine("Enter the number A :");
+//int number  = int.Parse(Console.ReadLine());
+//Fillnumber(number);
+
+Console.WriteLine("Enter the number A :");
+int A  = int.Parse(Console.ReadLine()?? "0");
+while(A<0)
+{
+    Console.WriteLine("It was entered fals value, please try again");
+    A  = int.Parse(Console.ReadLine()?? "0");
+}
+int count = 1;
+int summ = 0;
+
+while(count <= A)
+{
+    summ += count;
+    count += 1;
+
+}
+Console.WriteLine($"Summ of number from 1 to A =  {summ} " );
